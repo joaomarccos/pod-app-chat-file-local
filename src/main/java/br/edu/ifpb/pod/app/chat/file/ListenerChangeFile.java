b7eb500b6050c7680149832b5e991c0ec3106c86
emanuelbatista <emanuelbatista2011@gmail.com>
2015-05-04 17:15:40 -0300

    adicionando GUI principal

---
+++ src/main/java/br/edu/ifpb/pod/app/chat/file/ListenerChangeFile.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.pod.app.chat.file;
+
+/**
+ *
+ * @author Emanuel Batista da Silva Filho
+ */
+public interface ListenerChangeFile {
+    
+    public void notifyChange ();
+}
